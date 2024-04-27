d5200f8e761b4fcae0dc6dd542e483440bd96408
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-04-27 22:42:55 +0700

    feat(depedencies): add quill and tailwind typography depedencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,5 +76,5 @@ export default withUt({
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } as Config);
