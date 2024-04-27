2ee04ed9698da85081c40eced97ac541fe850ed8
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-04-27 23:03:04 +0700

    feat: configuration for uploadthings and quill

---
+++ constants/quill.ts
@@ -0,0 +1,26 @@
+export const quillModules = {
+  toolbar: [
+    [{ header: [1, 2, 3, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link"],
+    [{ align: [] }],
+    [{ color: [] }],
+    ["code-block"],
+  ],
+};
+
+export const quillFormats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "link",
+  "align",
+  "color",
+  "code-block",
+];
