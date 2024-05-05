51dca64ee5a70124b5a92f5099ee138b1ea21cf7
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-05-06 00:05:03 +0700

    refactor(components): data-table-loader to accept tableLength prop

--- components/common/data-table-loader.tsx
+++ components/common/data-table-loader.tsx
@@ -1,11 +1,17 @@
 import { TableCell, TableRow } from "@/components/ui/table";
 
-const DataTableLoader = () => {
+interface DataTableLoaderProps {
+  tableLength?: number;
+}
+
+const DataTableLoader: React.FC<DataTableLoaderProps> = (props) => {
+  const { tableLength = 3 } = props;
+
   return (
     <>
       {Array.from({ length: 10 }).map((_, index) => (
         <TableRow key={index}>
-          {Array.from({ length: 3 }).map((_, index) => (
+          {Array.from({ length: tableLength }).map((_, index) => (
             <TableCell key={index}>
               <div className="h-5 w-3/4 rounded bg-gray-100"></div>
             </TableCell>
